619d0af2dae42e81c6604b5679d192d8ab0d7f12
fanpeiyao <952568789@qq.com>
2017-08-31 16:26:39 +0800

    0830-3

--- src/app/services/services.js
+++ src/app/services/services.js
@@ -1,11 +1,19 @@
 
 
 angular.module("myApp")
-    .service("ProductService",function(){
 
+    .factory("ManService",function ($http,$q) {
 
+        var q=$q.defer();
+        var obj=q.promise;
+
+        $http({url:"../resource/resource.json",method:"get"}).then(function (e) {
+            q.resolve(e);
+        })
+        return obj;
 
     })
+
     .service("PublicService",function(){
 
 
