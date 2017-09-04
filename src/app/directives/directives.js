960ac5fab19654ae8ca89b1e58ceac9905baf746
fanpeiyao <952568789@qq.com>
2017-09-04 16:38:38 +0800

    0903-3...

--- src/app/directives/directives.js
+++ src/app/directives/directives.js
@@ -1,3 +1,41 @@
 
+angular.module('myApp')
+    .directive('toggleClass',function () {
+        return {
+            restrict:'A',
+            scope:{
+                toggleClass:'@'
+            },
+            link:function (scope,element) {
+                element.on('click',function () {
+                    element.toggleClass(scope.toggleClass);
+                })
+            }
+        }
+    })
 
+    .directive('listActive',function () {
+        return {
+            restrict:'A',
+            scope:{
+                listActive:'@'
+            },
+            link:function (scope,element) {
+                var list = angular.element('#list');
+                element.on('click',function () {
+
+                    list.find('.list-el').removeClass('inactive_reverse active_reverse').addClass('inactive');
+                    list.removeClass('rotate rotate_reverse').addClass('rotate');
+                    list.find('.overlay').removeClass('active active_reverse');
+                    angular.element(this).removeClass('inactive');
+                    angular.element(this).addClass('active');
+
+
+
+                    var o_target = angular.element(this).data('id');
+                    angular.element('#'+o_target).addClass('active');
+                })
+            }
+        }
+    })
 
